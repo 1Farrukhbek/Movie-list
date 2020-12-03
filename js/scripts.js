74bc30b399278ceed4a43311e6369f40cacdba47
Farrukhbek <68499298+1Farrukhbek@users.noreply.github.com>
2020-12-03 23:49:37 +0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var API_KEY = '949e950a';
+var elFormMovie = document.querySelector('.js-movie-form');
+var elBekorchiButton = elFormMovie.querySelector('.bekorchi-button');
+var elMovieInput = elFormMovie.querySelector('.js-movie-input');
+
+elFormMovie.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  var SEARCH_QUERY = elMovieInput.value.trim();
+
+  fetch(`http://omdbapi.com/?apikey=${API_KEY}&s=${SEARCH_QUERY}`).then(function (response) {
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
+    return response.json();
+  }).then(function (data) {
+    console.log(data);
+    console.log(data.Search);
+    console.log(data.Search[0]);
+    console.log(data.totalResults);
+  });
+});
